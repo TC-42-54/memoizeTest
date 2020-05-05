871c62f280014c4b1067283a77b6f5cb064c91ac
Thomas <t.chezieres@gmail.com>
2020-05-05 21:25:55 +0100

    initial commit

---
+++ memoize.js
@@ -0,0 +1,80 @@
+"use strict";
+
+let arrayEquals = (arr1, arr2) => {
+    var isEquals = true;
+    if (arr1.length != arr2.length) return false;
+    
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] != arr2[i]) isEquals = false;
+    }
+    
+    return isEquals;
+};
+
+let memoize = function (fn) {
+    let cache = {};
+    let functionName = fn.name;
+    return (...args) => {
+        let result;
+        if (functionName in cache) {
+            cache[functionName].forEach(cachedResult => {
+                if (arrayEquals(args, cachedResult.args)) {
+                    result = cachedResult.result;
+                }
+            });
+            if (typeof result !== "undefined")
+            {
+                console.log("cached data found");
+                return result;
+            }
+        } else {
+            cache[functionName] = [];
+        }
+        result = fn.apply(null, args);
+        cache[functionName].push({
+            args: args,
+            result: result
+        });
+        return result;
+    };
+};
+
+let cache = function (fn) {
+    let cache = {};
+    let functionName = fn.name;
+    return (...args) => {
+        let result;
+        let hitCount = 0;
+        if (functionName in cache) {
+            cache[functionName].forEach(cachedResult => {
+                if (arrayEquals(args, cachedResult.args)) {
+                    result = cachedResult.result;
+                    cachedResult.hitCount++;
+                    hitCount = cachedResult.hitCount;
+                }
+            });
+            if (typeof result !== "undefined")
+            {
+                console.log("cached data found");
+                return {result, hitCount};
+            }
+        } else {
+            cache[functionName] = [];
+        }
+        result = fn.apply(null, args);
+        cache[functionName].push({
+            args: args,
+            result: result,
+            hitCount: 0
+        });
+        return {result, hitCount};
+    };
+}
+
+var multiply = cache(x => x * 2);
+var triple = cache(x => x * 2);
+
+console.log(multiply(2));
+console.log(multiply(2));
+console.log(triple(2));
+console.log(triple(2));
